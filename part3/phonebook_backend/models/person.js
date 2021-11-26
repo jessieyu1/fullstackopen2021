10da330b72c752bd5ceacf2cba912d14d096a7c3
Jessie Yu <yujiaxin2009@gmail.com>
2021-11-26 12:21:11 +0800

    finish 3.22

--- part3/phonebook_backend/models/person.js
+++ part3/phonebook_backend/models/person.js
@@ -1,41 +1,42 @@
-//3.14
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-param-reassign */
+// 3.14
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
+
 const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-        minLength: [2, 'must be at least 2 characters'],
-    },
-    number: {
-        type: String,
-        required: true,
-        unique: true,
-        minLength: [8, 'must be at least 8 numbers'],
-    }
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: [2, 'must be at least 2 characters'],
+  },
+  number: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: [8, 'must be at least 8 numbers'],
+  },
 })
 personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
 })
 
 module.exports = mongoose.model('Person', personSchema)
-
-
