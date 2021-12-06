b8448b548266ad981587e7346a845923025fb90f
Jessie Yu <yujiaxin2009@gmail.com>
2021-12-06 19:34:47 +0800

    finish exercise 4.2
    Refactor the application into separate modules

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -4,38 +4,5 @@ const app = express()
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 
-const mongoose = require('mongoose')
-
-
-
-// logger.info('connecting to', config.MONGODB_URI)
-
-// mongoose.connect(config.MONGODB_URI)
-//   .then(() => {
-//     logger.info('connected to MongoDB')
-//   })
-//   .catch((error) => {
-//     logger.error('error connecting to MongoDB:', error.message)
-//   })
-
-// console.log('connecting to', mongoUrl)
-// mongoose.connect(mongoUrl)
-// const cors = require('cors')
-// const mongoose = require('mongoose')
-// app.use(cors())
-// app.use(express.json())
-
-// const blogSchema = new mongoose.Schema({
-//   title: String,
-//   author: String,
-//   url: String,
-//   likes: Number
-// })
-
-// const Blog = mongoose.model('Blog', blogSchema)
-
-
-
-
 
 logger.info(`Server running on port ${config.PORT}`)
